1037e5638609f1c3ae0100519eaf062c60762003
Mohammad Aditya Baihaqi <144421054+AdityaBaihaqi@users.noreply.github.com>
2023-11-14 21:54:02 +0700

    Add files via upload

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+    return (
+        <div className={styles.container}>
+            <nav className={styles.navbar}>
+                <div>
+                    <h1 className={styles.navbar__brand}>Netflix Indonesia</h1>
+                </div>
+                <div>
+                    <ul className={styles.navbar__list}>
+                        <li className={styles.navbar__item}>Home</li>
+                        <li className={styles.navbar__item}>Add Movie</li>
+                        <li className={styles.navbar__item}>Popular</li>
+                        <li className={styles.navbar__item}>Now Playing</li>
+                        <li className={styles.navbar__item}>Top Rated</li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
